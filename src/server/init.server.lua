0a1ccb62e999bfd0ffaa481bfc4c44d8e986fda1
Brandon <tysonnn92@gmail.com>
2023-04-26 19:15:03 -0400

    new file

--- src/server/init.server.lua
+++ src/server/init.server.lua
@@ -1,1 +1,8 @@
-print("Hello world, from server!")
+print("Hello world, from server!!!")
+local part = game.Workspace.part
+
+ part.Touched:Connect(function(touched)
+    
+    print("I was Touched")
+    
+ end)
